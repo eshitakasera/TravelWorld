0cd0e66951ee6ef0760ea1f722d59c2bd20f8c1d
eshitakasera <eshita.kasera70@gmail.com>
2026-01-20 23:53:57 +0530

    Update listing.js

--- models/models/listing.js
+++ models/models/listing.js
@@ -1,15 +1,49 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
-const ListingSchema = new Schema({
-    title: String,
-    description: String,
-    image: {
-        filename: String,
-        url: String
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    url:String,
+    filename: String,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
     },
-    price: Number,
-    location: String,
-    country: String
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  category: {
+    type: String, 
+    required: true,
+  },
 });
-const Listing =mongoose.model("listing",ListingSchema);
-module.exports=Listing;
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
