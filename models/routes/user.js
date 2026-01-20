084200e5591447bd322763d42275b9aa0446bffd
eshitakasera <eshita.kasera70@gmail.com>
2026-01-21 00:55:07 +0530

    Update user.js
    Add user authentication routes

--- models/routes/user.js
+++ models/routes/user.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
-const wrapAsync = require("../utils/wrapAsync.js");
-const ExpressError = require("../utils/ExpressError.js");
+const User = require("../models/models/user");
+const wrapAsync = require("../models/utils/wrapAsync.js");
+const ExpressError = require("../models/utils/ExpressError.js");
 const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
-const reviewController = require("../controllers/users.js");
+const { saveRedirectUrl } = require("../models/middleware.js");
+const reviewController = require("../models/controllers/users.js");
 
 router
   .route("/signup")
